4178b71364fb86ac72b046820ae70adb069b6fac
thainhatanh61 <thainhatanh61@gmail.com>
2026-05-06 23:55:11 +0700

    add Peashooter and Pea projectile classes

--- src/Pea.java
+++ src/Pea.java
@@ -1,3 +1,30 @@
+import java.awt.*;
+
 public class Pea {
-    
+    private int x,y;
+    private int speed;
+    public Pea(int x,int y){
+        this.x=x;
+        this.y=y;
+        this.speed=5;
+    }
+    public void update(){
+        x+=speed;
+    }
+    public void draw(Graphics g){
+        g.setColor(Color.GREEN);
+        g.fillOval(x-5, y-5, 10, 10);
+    }
+    public boolean hits(Zombie zombie){
+        return Math.abs(x-zombie.getX())<30&& Math.abs(y-zombie.getY())<30;
+    }
+    public int getX(){
+        return x;
+    }
+    public int getY() {
+        return y;
+    }
+    public boolean isOffScreen(){
+        return x>900;
+    }
 }
--- src/Peashooter.java
+++ src/Peashooter.java
@@ -1,3 +1,50 @@
-public class Peashooter {
-    
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Peashooter extends Plant{
+    private List<Pea> peas;
+    private int shootTimer;
+    private int row;
+    public Peashooter(int x,int y, int row){
+        super(x,y,100,200);
+        this.row=row;
+        peas = new ArrayList<>();
+        shootTimer=0;
+    }
+    @Override
+    public void update(List<Zombie> zombies, List<Sun> suns){
+        shootTimer++;
+        if (shootTimer>=60){
+            shootTimer=0;
+            peas.add(new Pea(x+10,y-5));
+        }
+        for (int i=0; i<peas.size();i++){
+            peas.get(i).update();
+            boolean hit=false;
+            for (Zombie zombie: zombies){
+                if (!zombie.isDead()&& peas.get(i).hits(zombie)){
+                    zombie.takeDamage(20);
+                    hit =true;
+                    break;
+                }
+            }
+            if (hit||peas.get(i).isOffScreen()){
+                peas.remove(i);
+                i--;
+            }
+        }
+    }
+    @Override
+    public void draw(Graphics g){
+        g.setColor(new Color(34,180,34));
+        g.fillOval(x-18, y-30, 36, 36);
+        g.setColor(new Color(0, 130, 0));
+        g.fillRect(x + 10, y - 12, 20, 10);
+        g.setColor(Color.GREEN);
+        g.fillRect(x - 3, y + 6, 6, 18);
+        for (Pea pea : peas) {
+            pea.draw(g);
+        }
+    }
 }
