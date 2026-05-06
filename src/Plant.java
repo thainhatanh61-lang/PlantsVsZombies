1b54de6d398084d973e80a738593b7b144bce723
thainhatanh61 <thainhatanh61@gmail.com>
2026-05-06 23:05:46 +0700

    add Plant abstract class

--- src/Plant.java
+++ src/Plant.java
@@ -1,3 +1,34 @@
-public class Plant {
-    
+import java.awt.*;
+import java.util.List;
+
+public abstract class Plant {
+    protected int x,y;
+    protected int health;
+    protected int cost;
+    public Plant(int x,int y,int cost, int health){
+        this.x = x;
+        this.y = y;
+        this.cost = cost;
+        this.health = health;
+    }
+    public abstract void update(List<Zombie> zombies, List<Sun> suns);
+    public abstract void draw(Graphics g);
+    public void takeDamege(int damge){
+        health-=damege;
+    }
+    public int getX() {
+        return x;
+    }
+    public int getY() {
+        return y;
+    }
+    public int getCost() {
+        return cost;
+    }
+    public int getHealth() {
+        return health;
+    }
+    public boolean isDead(){
+        return health<=0;
+    }
 }
