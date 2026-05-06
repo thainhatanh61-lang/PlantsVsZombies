bbac6e692a6efd4031d3cfe12a89094b4a91ec58
thainhatanh61 <thainhatanh61@gmail.com>
2026-05-06 23:18:58 +0700

    add Sunflower class that produces sun

--- src/Sunflower.java
+++ src/Sunflower.java
@@ -1,3 +1,33 @@
-public class Sunflower {
-    
+import java.awt.*;
+import java.util.List;
+
+public class Sunflower extends Plant{
+    private int timer=0;
+    public Sunflower(int x, int y){
+        super(x,y,50,100);
+    }
+    @Override
+    public void update(List<Zombie> zombies, List<Sun> suns){
+        timer++;
+        if (timer>=300){
+            timer=0;
+            suns.add(new Sun(x,y,y-40));
+        }
+    }
+    @Override
+    public void draw(Graphics g) {
+        // Yellow flower head
+        g.setColor(Color.YELLOW);
+        g.fillOval(x - 20, y - 35, 40, 40);
+        // Brown center
+        g.setColor(new Color(139, 90, 43));
+        g.fillOval(x - 10, y - 25, 20, 20);
+        // Green stem
+        g.setColor(Color.GREEN);
+        g.fillRect(x - 3, y + 5, 6, 20);
+        // Label
+        g.setColor(Color.BLACK);
+        g.setFont(new Font("Arial", Font.PLAIN, 8));
+        g.drawString("Sun", x - 10, y + 5);
+    }
 }
